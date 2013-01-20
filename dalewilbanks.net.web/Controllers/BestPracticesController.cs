f10718df541d1293bdac02995ccf4c68c0045dbf
Dale Wilbanks <dalewilbanks@gmail.com>
2013-01-20 16:53:50 -0500

    Converted Best Practices and it's sub views to WorldPress

--- dalewilbanks.net.web/Controllers/BestPracticesController.cs
+++ dalewilbanks.net.web/Controllers/BestPracticesController.cs
@@ -11,23 +11,25 @@ namespace dalewilbanks.net.web.Controllers
         public ActionResult Index()
         {
             ViewBag.Content = dalewilbanks.net.wordpress.WordPressReader.Read("http://dalewilbanks.wordpress.com/feed/", "Best Practices");
-
             return View("BestPractices");
         }
 
         public ActionResult Methodology()
         {
+            ViewBag.Content = dalewilbanks.net.wordpress.WordPressReader.Read("http://dalewilbanks.wordpress.com/feed/", "Methodology");
             return View("Methodology");
         }
 
         public ActionResult Usability()
         {
+            ViewBag.Content = dalewilbanks.net.wordpress.WordPressReader.Read("http://dalewilbanks.wordpress.com/feed/", "Usability");
             return View("Usability");
         }
 
 
         public ActionResult DesignPatterns()
         {
+            ViewBag.Content = dalewilbanks.net.wordpress.WordPressReader.Read("http://dalewilbanks.wordpress.com/feed/", "Design Patterns");
             return View("DesignPatterns");
         }
 
