07522a9241bd9a56b653e243a84fd14dcfc48217
Dale Wilbanks <dalewilbanks@gmail.com>
2013-02-17 11:56:16 -0500

    converted external link to a partial view, so that it can be rendered across multiple viewers.  @helper only worked on the current page, and could not be reused across views well.

--- dalewilbanks.net.web/Controllers/CSS3.cs
+++ dalewilbanks.net.web/Controllers/CSS3.cs
@@ -16,90 +16,9 @@ namespace dalewilbanks.net.web.Controllers
             return View("CSS3");
         }
 
-        //
-        // GET: /CSS3/Details/5
-
-        public ActionResult Details(int id)
+        public ActionResult CSSConcepts()
         {
-            return View();
-        }
-
-        //
-        // GET: /CSS3/Create
-
-        public ActionResult Create()
-        {
-            return View();
-        }
-
-        //
-        // POST: /CSS3/Create
-
-        [HttpPost]
-        public ActionResult Create(FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add insert logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /CSS3/Edit/5
-
-        public ActionResult Edit(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /CSS3/Edit/5
-
-        [HttpPost]
-        public ActionResult Edit(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add update logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
-        }
-
-        //
-        // GET: /CSS3/Delete/5
-
-        public ActionResult Delete(int id)
-        {
-            return View();
-        }
-
-        //
-        // POST: /CSS3/Delete/5
-
-        [HttpPost]
-        public ActionResult Delete(int id, FormCollection collection)
-        {
-            try
-            {
-                // TODO: Add delete logic here
-
-                return RedirectToAction("Index");
-            }
-            catch
-            {
-                return View();
-            }
+            return View("CSSConcepts");
         }
     }
 }
