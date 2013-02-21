1b13d357865f8d4f90e6a0bb0d82f9702ea7e8d8
Dale Wilbanks <dalewilbanks@gmail.com>
2013-02-20 19:38:13 -0500

    Created views for each step

--- dalewilbanks.net.web/Controllers/CSS3.cs
+++ dalewilbanks.net.web/Controllers/CSS3.cs
@@ -18,7 +18,37 @@ namespace dalewilbanks.net.web.Controllers
 
         public ActionResult CSSConcepts()
         {
-            return View("CSSConcepts");
+            return View("1-CSSConcepts/CSSConcepts");
+        }
+
+        public ActionResult PrototypeLayout()
+        {
+            return View("2-PrototypeLayout/PrototypeLayout");
+        }
+
+        public ActionResult KnowYourTools()
+        {
+            return View("3-KnowYourTools/KnowYourTools");
+        }
+
+        public ActionResult MaintainableCSS()
+        {
+            return View("4-MaintainableCSS/MaintainableCSS");
+        }
+
+        public ActionResult GraphicsAndColorScheme()
+        {
+            return View("5-GraphicsAndColorScheme/GraphicsAndColorScheme");
+        }
+
+        public ActionResult DefineStyles()
+        {
+            return View("6-DefineStyles/DefineStyles");
+        }
+
+        public ActionResult AdvancedCSS3Effects()
+        {
+            return View("7-AdvancedCSS3Effects/AdvancedCSS3Effects");
         }
     }
 }
