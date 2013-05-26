de6001583e39a17a8162bccd5594baef7f6595b0
Dale Wilbanks <dalewilbanks@gmail.com>
2013-05-25 23:19:38 -0400

    Added AngularJS starter page

---
+++ dalewilbanks.net.web/Controllers/AngularJSController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace dalewilbanks.net.web.Controllers
+{
+    public class AngularJSController : Controller
+    {
+        //
+        // GET: /AngularJS/
+
+        public ActionResult Index()
+        {
+            return View("AngularJS");
+        }
+
+    }
+}
